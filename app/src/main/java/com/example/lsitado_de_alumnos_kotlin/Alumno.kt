a25dd0d2bda2d565efe4adbe3e0f95c03ba50e21
estefania27am <79113403+Estefania2709@users.noreply.github.com>
2023-09-29 23:30:31 -0600

    Add files via upload

---
+++ app/src/main/java/com/example/lsitado_de_alumnos_kotlin/Alumno.kt
@@ -0,0 +1,8 @@
+package com.example.lsitado_de_alumnos_kotlin
+
+data class Alumno(
+    val nombre: String,
+    val cuenta: String,
+    val imagen: Int,
+    val correo: String
+)
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lsitado_de_alumnos_kotlin"
+include(":app")
